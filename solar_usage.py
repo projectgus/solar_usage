1b2498c7b822fd85ab617bc4d5a43f9718cd60f5
Angus Gratton <gus@projectgus.com>
2021-04-22 08:34:19 +1000

    use class for numeric display part

--- solar_usage.py
+++ solar_usage.py
@@ -1,4 +1,6 @@
-import urequests, ugfx, network, badge, time
+import urequests
+import ugfx
+import time
 import json
 
 # Some layout dimensions
@@ -13,18 +15,26 @@ XAXIS_Y = HEIGHT-14  # X axis horizontal line
 X_WIDTH = WIDTH-LINE_X  # 278, 1 minute per pixel
 Y_HEIGHT = XAXIS_Y-LINE_Y
 
-def close_values(a,b):
-    return abs((a or 0) - (b or 0)) < 0.2
-
 
 class Sample(object):
-    def __init__(self, ts, solar, grid):
+    def __init__(self, ts, solar, usage):
         self.ts = ts
         self.solar = solar
-        self.grid = grid
+        self.usage = usage
+
+    def __repr__(self):
+        return "{}: {} / {}".format(self.ts, self.solar, self.usage)
 
     def is_empty(self):
-        return self.solar is None and self.grid is None
+        return self.solar is None and self.usage is None
+
+    def close_to(self, b):
+        def close_values(a, b):
+            return abs((a or 0) - (b or 0)) < 0.2
+
+        if b is None:
+            return False
+        return close_values(self.solar, b.solar) and close_values(self.usage, b.usage)
 
 
 class NumberDisplay(object):
@@ -37,46 +47,41 @@ class NumberDisplay(object):
 
     def __init__(self):
         self.num_updates = 0  # trigger a full redraw next
-        self.current_solar = None
-        self.current_grid = None
+        self.last_sample = None
         self.redraw_display()
-
 
     def redraw_display(self):
         for _ in range(3):
             # cycle the whole region to refresh the e-ink pixels
-            ugfx.Imagebox(0, LINE_Y, WIDTH, LINE_Y, ugfx.BLACK)
+            ugfx.area(0, 0, WIDTH, LINE_Y, ugfx.BLACK)
             ugfx.flush()
-            ugfx.Imagebox(0, LINE_Y, WIDTH, LINE_Y, ugfx.WHITE)
+            ugfx.area(0, 0, WIDTH, LINE_Y, ugfx.WHITE)
             ugfx.flush()
 
         # line under the display, icons
-        ugfx.line(0,LINE_Y,WIDTH-1,LINE_Y,ugfx.BLACK)
+        ugfx.line(0, LINE_Y, WIDTH - 1, LINE_Y, ugfx.BLACK)
         ugfx.Imagebox(0, 0, 32, 32, './sun.png')
         ugfx.Imagebox(130, 0, 32, 32, './house.png')
         ugfx.flush()
 
-
     def update(self, sample):
-        if close_values(sample.solar, self.current_solar) and close_values(sample.grid, self.current_grid):
-            return  # if not much changed, don't bother showing it
+        if sample.close_to(self.last_sample):
+            return  # if not much changed, don't bother updating this part
+        self.last_sample = sample
 
-        if self.num_updates == REDRAW_UPDATES:
+        if self.num_updates == self.REDRAW_UPDATES:
             self.redraw_display()  # do a full refresh
             self.num_updates = 0
         else:
             self.num_updates += 1
 
-        def display_wattage(value):
+        def as_text(value):
             return "-" if value is None else "{:.1f}W".format(value)
-
-        solar = display_wattage(sample.solar)
-        grid = display_wattage(sample.grid)
 
         ugfx.area(36, 4, 130-36, 32-4, ugfx.WHITE)
         ugfx.area(166, 4, WIDTH-166, 32-4, ugfx.WHITE)
-        ugfx.string(36, 4, solar, 'Roboto_Regular22', ugfx.BLACK)
-        ugfx.string(166, 4, grid, 'Roboto_Regular22', ugfx.BLACK)
+        ugfx.string(36, 4, as_text(sample.solar), 'Roboto_Regular22', ugfx.BLACK)
+        ugfx.string(166, 4, as_text(sample.usage), 'Roboto_Regular22', ugfx.BLACK)
         ugfx.flush()
 
 
@@ -92,12 +97,6 @@ def uri_encode(seq):
 
 
 def redraw_display():
-    ugfx.clear(ugfx.WHITE)
-    ugfx.Imagebox(0, 0, 32, 32, './sun.png')
-    ugfx.Imagebox(130, 0, 32, 32, './house.png')
-    ugfx.flush()
-
-    # line under full wattage
     ugfx.line(LINE_X, LINE_Y, LINE_X, XAXIS_Y, ugfx.BLACK)
     ugfx.line(0, XAXIS_Y, WIDTH-1, XAXIS_Y, ugfx.BLACK)
 
@@ -124,22 +123,22 @@ def draw_graph(samples):
     max_power = get_max_power(samples)
 
     # draw all points
-    last_p = samples[0][2] or 0
+    last_p = samples[0].usage or 0
     last_x = LINE_X
     last_y = int(Y_HEIGHT - (last_p / max_power) * Y_HEIGHT)
     TS_SCALAR = 15  # this many seconds per pixel
-    ORIGIN_TS = (samples[-1][0] // X_WIDTH) * X_WIDTH // TS_SCALAR
-    print("timestamps from {} to {}".format(samples[0][0], samples[-1][0]))
+    ORIGIN_TS = (samples[-1].ts // X_WIDTH) * X_WIDTH // TS_SCALAR
+    print("timestamps from {} to {}".format(samples[0].ts, samples[-1].ts))
     print("graph range from {} to {}".format(ORIGIN_TS, ORIGIN_TS + X_WIDTH))
     print(X_WIDTH)
-    for ts,solar,power in samples:
-        ts //= TS_SCALAR
-        if ts < ORIGIN_TS:
+    for s in samples:
+        ts = s.ts / TS_SCALAR
+        if s.ts < ORIGIN_TS:
             continue
         x = ts - ORIGIN_TS + LINE_X
-        if power is None:
+        if s.usage is None:
             continue
-        y = int(Y_HEIGHT - (power / max_power) * Y_HEIGHT)
+        y = int(Y_HEIGHT - (s.usage / max_power) * Y_HEIGHT)
         ugfx.line(last_x, last_y, x, y, ugfx.BLACK)
         last_x = x
         last_y = y
@@ -148,32 +147,38 @@ def draw_graph(samples):
 def main():
     ugfx.init()
 
+    numbers = NumberDisplay()
+
     redraw_display()
 
     r = query_data('now() - 60m')
     print(r)
-    latest_ts, latest_solar, latest_grid = r[-1]
-    draw_current_wattage(latest_solar, latest_grid)
+
+    if len(r):
+        numbers.update(r[-1])
 
     draw_graph(r)
 
     while True:
+        print("in main loop")
         time.sleep(5)
-        r = query_data('{}s'.format(latest_ts), '5s')
+        print(r[-1])
+        print("sampling since {}".format(r[-1].ts))
+        return
+        r = query_data('{}s'.format(r[-1].ts), '5s')
         if r:
-            latest_ts, latest_solar, latest_grid = r[-1]
-            print(latest_ts, latest_solar, latest_grid)
-            if latest_solar is not None or latest_grid is not None:
-                draw_current_wattage(latest_solar, latest_grid)
+            latest = r[-1]
+            print(latest.ts, latest.solar, latest.usage)
+            numbers.update(latest)
 
 
 def get_max_power(samples):
     try:
-        max_solar = max(solar for _,solar,_ in samples if solar)
+        max_solar = max(s.solar for s in samples if s.solar)
     except ValueError:
         max_solar = None
     try:
-        max_load = max(grid for _,_,grid in samples if grid)
+        max_load = max(s.usage for s in samples if s.usage)
     except ValueError:
         max_load = None
     try:
@@ -185,13 +190,14 @@ def get_max_power(samples):
 
 def query_data(since, group_by='5s'):
     # returns list of 3-lists [timestamp, solar, load]
-    query = uri_encode('SELECT mean(solar),mean(load)*-1 from power where time > {} group by time({})'.format(since, group_by))
+    query = uri_encode('SELECT mean(solar),mean(load)*-1 from power where '
+                       'time > {} group by time({})'.format(since, group_by))
 
     # spiky.lan
     resp = urequests.post('http://192.168.66.1:8086/query?db=sensors&epoch=s',
                           data=b'q='+query,
                           headers={
-                          'Content-Type': 'application/x-www-form-urlencoded'
+                              'Content-Type': 'application/x-www-form-urlencoded'
                           })
     text = resp.text
     resp.close()
@@ -200,7 +206,7 @@ def query_data(since, group_by='5s'):
         return []
     data = json.loads(text)
     result = [Sample(*x) for x in data['results'][0]['series'][0]['values']]
-    result = [s for s in result if not s.is_empty]   # remove all the empty samples
+    result = [s for s in result if not s.is_empty()]   # remove all the empty samples
     return result
 
 
