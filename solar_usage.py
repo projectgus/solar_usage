06d05305c71e018c0d1e94f9af7bc3746e719f9c
Angus Gratton <gus@projectgus.com>
2021-04-22 21:36:12 +1000

    mostly working

--- solar_usage.py
+++ solar_usage.py
@@ -12,13 +12,10 @@ LINE_X = 18  # Y axis vertical line
 
 XAXIS_Y = HEIGHT-14  # X axis horizontal line
 
-X_WIDTH = WIDTH-LINE_X  # 278, 1 minute per pixel
-Y_HEIGHT = XAXIS_Y-LINE_Y
-
 
 class Sample(object):
     def __init__(self, ts, solar, usage):
-        self.ts = ts
+        self.ts = int(ts)
         self.solar = solar
         self.usage = usage
 
@@ -27,6 +24,9 @@ class Sample(object):
 
     def is_empty(self):
         return self.solar is None and self.usage is None
+
+    def max_power(self):
+        return max(self.solar or 0, self.usage or 0)
 
     def close_to(self, b):
         def close_values(a, b):
@@ -51,8 +51,7 @@ class NumberDisplay(object):
         self.redraw_display()
 
     def redraw_display(self):
-        for _ in range(3):
-            # cycle the whole region to refresh the e-ink pixels
+        for _ in range(1):  # todo make this cycle to refresh e-ink
             ugfx.area(0, 0, WIDTH, LINE_Y, ugfx.BLACK)
             ugfx.flush()
             ugfx.area(0, 0, WIDTH, LINE_Y, ugfx.WHITE)
@@ -85,6 +84,129 @@ class NumberDisplay(object):
         ugfx.flush()
 
 
+class Graph(object):
+    """
+    Bottom half of display shows a historical data graph
+
+    Draws to the display from (0,0) to (WIDTH-1, LINE_Y), inclusive
+    """
+    TS_SCALAR = 15  # this many seconds per pixel
+    X_WIDTH = WIDTH-LINE_X  # 278, 1 minute per pixel
+    Y_HEIGHT = XAXIS_Y-LINE_Y
+
+    def __init__(self):
+        self.num_updates = 0  # trigger a full redraw next
+        self.samples = []
+        self.max_power = 5000  # determines the scale of the graph in Watts, update() will recalc it
+        self.origin_ts = None  # timestamp that correlates to the left-hand side of the graph, update() will recalc it
+        self.last_x = None
+        self.last_usage_y = None
+        self.last_solar_y = None
+        self.redraw_display()
+
+    def redraw_display(self):
+        # draw the X & Y axis lines
+
+        # todo: do the binky thing here too
+        ugfx.area(0, LINE_Y, WIDTH, HEIGHT-LINE_Y, ugfx.WHITE)
+        ugfx.line(LINE_X, LINE_Y, LINE_X, XAXIS_Y, ugfx.BLACK)
+        ugfx.line(0, XAXIS_Y, WIDTH-1, XAXIS_Y, ugfx.BLACK)
+        ugfx.flush()
+
+        # draw the graph Y axis
+        step_height = 15
+        steps = 6
+        watts_per_step = self.max_power // steps
+        for step in range(steps):
+            y = XAXIS_Y - step * step_height
+            if step % 2 == 1:
+                from_x = LINE_X-4
+                # to_x = LINE_X
+                ugfx.string(0, y - 6, "{:.1f}".format(step * watts_per_step / 1000), "", ugfx.BLACK)
+            else:
+                from_x = LINE_X - 3
+                # to_x = LINE_X - 1
+            ugfx.line(from_x, y, LINE_X, y, HEIGHT-1)
+        ugfx.flush()
+
+        if not self.samples:
+            self.origin_ts = None
+            return  # nothing else to draw, leave the X axis and graph area blank
+
+        self.last_x = None
+        self.last_solar_y = None
+        self.last_usage_y = None
+        self.draw_samples(self.samples)  # draw the full graph!
+
+    def update(self, samples):
+        for new_sample in samples:
+            # add the new samples to the current list of samples
+            # (assuming they come in order, but possibly some new samples are dups)
+            changed = False
+            if not self.samples or self.samples[-1].ts < new_sample.ts:
+                self.samples.append(new_sample)
+                changed = True
+        if not changed:
+            return  # nothing new, nothing to do
+
+        WIDTH_SECONDS = (self.X_WIDTH * self.TS_SCALAR)
+        SCROLL_SECONDS = WIDTH_SECONDS // 4
+        new_origin = ((self.samples[-1].ts + 1) // SCROLL_SECONDS) * SCROLL_SECONDS \
+            - 3 * SCROLL_SECONDS
+        print("graph timestamp range {} - {} ({} seconds)".format(
+            new_origin, new_origin + WIDTH_SECONDS, WIDTH_SECONDS))
+        while self.samples[0].ts < new_origin:
+            del self.samples[0]
+
+        # there should be at least one sample in the graph at this point (or above will error out)
+
+        new_max = int(max(s.max_power() for s in self.samples) * 1.05)  # 5% headroom on graph
+        if new_origin != self.origin_ts or new_max != self.max_power:
+            print("origin {} -> {} max {} -> {}, redraw!".format(
+                self.origin_ts, new_origin, self.max_power, new_max))
+            # need to draw the whole graph again!
+            self.origin_ts = new_origin
+            self.max_power = new_max
+            self.redraw_display()
+        else:
+            # just draw the new samples here, onto the existing graph
+            self.draw_samples(samples)
+
+    def draw_samples(self, samples):
+        print("Drawing {} samples".format(len(samples)))
+
+        def value_to_y(value):
+            assert value < self.max_power
+            result = (value / self.max_power) * self.Y_HEIGHT
+            return self.Y_HEIGHT - int(result) + LINE_Y
+
+        for s in samples:
+            x = (s.ts - self.origin_ts) // self.TS_SCALAR + LINE_X
+            if self.last_x is None:
+                self.last_x = x
+
+            solar_y = None
+            usage_y = None
+            if s.solar:
+                solar_y = value_to_y(s.solar)
+                if self.last_solar_y is None:
+                    self.last_solar_y = solar_Y
+                ugfx.line(self.last_x, self.solar_y, x, solar_y, ugfx.GREY)  # color??
+                self.last_solar_y = solar_y
+            if s.usage:
+                usage_y = value_to_y(s.usage)
+                if self.last_usage_y is None:
+                    self.last_usage_y = usage_y
+                ugfx.line(self.last_x, self.last_usage_y, x, usage_y, ugfx.BLACK)
+                self.last_usage_y = usage_y
+            self.last_x = x
+            print(s,x,solar_y,usage_y)
+        ugfx.flush()
+
+    def sample_y(self, value):
+        return result
+
+
 def uri_encode(seq):
     resp = b''
     for c in seq.encode():
@@ -96,80 +218,25 @@ def uri_encode(seq):
     return resp
 
 
-def redraw_display():
-    ugfx.line(LINE_X, LINE_Y, LINE_X, XAXIS_Y, ugfx.BLACK)
-    ugfx.line(0, XAXIS_Y, WIDTH-1, XAXIS_Y, ugfx.BLACK)
-
-    ugfx.flush()
-
-    # draw the graph Y axis
-    step_height = 15
-    steps = 6
-    kw_per_step = 1
-    for step in range(steps):
-        y = XAXIS_Y - step * step_height
-        if step % 2 == 1:
-            from_x = LINE_X-4
-            to_x = LINE_X
-            ugfx.string(0, y - 6, "{:.1f}".format(step * kw_per_step), "", ugfx.BLACK)
-        else:
-            from_x = LINE_X - 3
-            to_x = LINE_X - 1
-        ugfx.line(from_x, y, LINE_X, y, HEIGHT-1)
-    ugfx.flush()
-
-
-def draw_graph(samples):
-    max_power = get_max_power(samples)
-
-    # draw all points
-    last_p = samples[0].usage or 0
-    last_x = LINE_X
-    last_y = int(Y_HEIGHT - (last_p / max_power) * Y_HEIGHT)
-    TS_SCALAR = 15  # this many seconds per pixel
-    ORIGIN_TS = (samples[-1].ts // X_WIDTH) * X_WIDTH // TS_SCALAR
-    print("timestamps from {} to {}".format(samples[0].ts, samples[-1].ts))
-    print("graph range from {} to {}".format(ORIGIN_TS, ORIGIN_TS + X_WIDTH))
-    print(X_WIDTH)
-    for s in samples:
-        ts = s.ts / TS_SCALAR
-        if s.ts < ORIGIN_TS:
-            continue
-        x = ts - ORIGIN_TS + LINE_X
-        if s.usage is None:
-            continue
-        y = int(Y_HEIGHT - (s.usage / max_power) * Y_HEIGHT)
-        ugfx.line(last_x, last_y, x, y, ugfx.BLACK)
-        last_x = x
-        last_y = y
-
-
 def main():
     ugfx.init()
 
     numbers = NumberDisplay()
+    graph = Graph()
 
-    redraw_display()
-
-    r = query_data('now() - 60m')
-    print(r)
-
-    if len(r):
-        numbers.update(r[-1])
-
-    draw_graph(r)
+    samples = []
+    while not samples:
+        samples = query_data('now() - 60m')
+        print(samples)
 
     while True:
         print("in main loop")
+        if samples:
+            numbers.update(samples[-1])
+            graph.update(samples)
         time.sleep(5)
-        print(r[-1])
-        print("sampling since {}".format(r[-1].ts))
-        return
-        r = query_data('{}s'.format(r[-1].ts), '5s')
-        if r:
-            latest = r[-1]
-            print(latest.ts, latest.solar, latest.usage)
-            numbers.update(latest)
+        samples = query_data('{}s'.format(samples[-1].ts), '5s')
+        print("got {} samples".format(len(samples)))
 
 
 def get_max_power(samples):
